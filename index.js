c55f21c35f56c07d356cd1012c60040e7fd14b13
Garklein <garklein97@gmail.com>
2021-03-22 16:48:58 -0400

    Packet handler is its own file

--- index.js
+++ index.js
@@ -5,6 +5,7 @@
 // Constants
 const Room = require("./lib/room.js");
 const C = require("./lib/consts.js");
+const ph = require("./lib/packet-handler.js");
 
 const http = require("http");
 const path = require("path");
@@ -104,97 +105,8 @@ wss.on('connection', function connection(ws) {
 
     // Messages being received from that socket
     ws.on('message', function incoming(message) {
-
         let msg = JSON.parse(message);
-        switch (msg[0]) {
-
-            // Creating/joining rooms
-            case "a":
-                switch (msg[1]) {
-
-                    // Creating a room
-                    case "a":
-                        if (rooms.has(msg[2])) ws.send(JSON.stringify(["a", "a", "b", msg[2]]));
-                        else {
-                            if (myID !== undefined) {
-                                rooms.get(myRoom).players[myID] = undefined;
-                                myID = undefined;
-                            }
-                            rooms.get(myRoom).connected--;
-                            if (!rooms.get(myRoom).connected && myRoom !== "") rooms.delete(myRoom);
-                            rooms.set(msg[2], 
-                                new Room(
-                                    parseInt(msg[3]) === NaN ? 20 : Math.round(parseInt(msg[3])) - 1,
-                                    parseInt(msg[4]) === NaN ? 20 : Math.round(parseInt(msg[4])) - 1
-                                )
-                            );
-                            myRoom = msg[2];
-                            ws.send(JSON.stringify(["a", "a", "a", msg[2], rooms.get(msg[2]).info]));
-                        }
-                        break;
-
-                    // Joining a room
-                    case "b":
-                        if (rooms.has(msg[2])) {
-                            if (myID !== undefined) {
-                                rooms.get(myRoom).players[myID] = undefined;
-                                myID = undefined;
-                            }
-                            rooms.get(myRoom).connected--;
-                            if (!rooms.get(myRoom).connected && myRoom !== "") rooms.delete(myRoom);
-
-                            ws.send(JSON.stringify(["a", "b", "a", msg[2], rooms.get(msg[2]).info]));
-                            myRoom = msg[2];
-                            rooms.get(msg[2]).connected++;
-                        } else ws.send(JSON.stringify(["a", "b", "b", msg[2]]));
-                        break;
-                }
-                break;
-
-            // Spawning
-            case "b":
-                if (myID !== undefined) return;
-                const spawnReturn = rooms.get(myRoom).spawnPlayer(myName, msg.slice(1, 21), bruh, ws);
-                myID = spawnReturn[0];
-                myName = spawnReturn[1];
-                break;
-
-            // Button pressed or movement stuff
-            case "c": 
-                switch (msg[1]) {
-
-                    // keyup
-                    case "a":
-                        if (myID === undefined) return;
-                        rooms.get(myRoom).players[myID].keyDown(msg.slice(2, msg.length));
-                        break;
-
-                    // keydown
-                    case "b":
-                        if (myID === undefined) return;
-                        rooms.get(myRoom).players[myID].keyUp(msg.slice(2, msg.length))
-                        break;
-
-                    // changing movement settings
-                    case "c":
-                        bruh = !bruh;
-                        if (myID !== undefined) rooms.get(myRoom).players[myID].keyboard = !rooms.get(myRoom).players[myID].keyboard;
-                        break;
-
-                    // moving mouse
-                    case "d":
-                        if (myID === undefined) return;
-                        rooms.get(myRoom).players[myID].mouse.mouseX = msg[2];
-                        rooms.get(myRoom).players[myID].mouse.mouseY = msg[3];
-                        rooms.get(myRoom).players[myID].res = msg[4];
-                }
-                break;
-
-            // Ping
-            case "p":
-                if (msg === "ping") ws.send(JSON.stringify("pong"));
-                break;
-        }
+        [myRoom, myID, myName, bruh] = ph(msg, myRoom, myID, myName, rooms, bruh, ws);
     });
 });
 
