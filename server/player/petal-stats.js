4c21a91c872e5591953d756ed477b9af833d07e8
Garklein <garklein97@gmail.com>
2021-08-31 14:25:05 -0400

    petal template

--- server/player/petal-stats.js
+++ server/player/petal-stats.js
@@ -9,98 +9,35 @@ const d = (petal, mul, centre, rad) => { // default behavior
 }
 const no = () => {}; // noop
 
+// post is an action to be done after updating petal
+class PetalStat {
+	constructor({ radius, cooldown, damage, health, attack=d, defend=d, neutral=d, post=no, equip=no }) {
+		if (radius === undefined || cooldown === undefined || damage === undefined || health === undefined) {
+			throw "Tried to make petal with an undefined essential prop";
+		}
+		this.radius = radius;
+		this.cooldown = cooldown;
+		this.damage = damage;
+		this.health = health;
+		this.attack = attack; this.defend = defend; this.neutral = neutral;
+		this.post = post; this.equip = equip;
+	}
+}
+
 const petalStats = {
-	// haha's dev petal
-	"-3": {
-        radius: 16,
-        cooldown: 50,
-        damage: 30,
-        health: 30,
-		attack: d, defend: d, neutral: d, 
-		post: no, // action to be done after updating petal
-		equip: no
-    },
-
-    0: {
-        radius: 0,
-        cooldown: 0,
-        damage: 0,
-        hp: 0,
-		attack: d, defend: d, neutral: d,
-		post: no,
-		equip: no
-    },
-
-    // basic
-    1: {
-        radius: 10,
-        cooldown: 2500,
-        damage: 10,
-        hp: 10,
-		attack: d, defend: d, neutral: d,
-		post: no,
-		equip: no
-    },
-
-	// fast
-	2: {
-		radius: 8,
-		cooldown: 500,
-		damage: 8,
-		health: 5,
-		attack: d, defend: d, neutral: d,
-		post: no,
-		equip: no
-	},
-
-    // heavy
-	3: {
-		radius: 12,
-		cooldown: 5500,
-		damage: 20,
-		health: 20,
-		attack: d, defend: d, neutral: d,
-		post: no,
-		equip: no
-	},
-
-    // iris
-    4:{
-        radius: 6,
-		cooldown: 6000,
-		damage: 5,
-		health: 5,
-		attack: d, defend: d, neutral: d,
-		post: no,
-		equip: no
-    },
-
-    // rose
-    7:{
-        radius: 11,
-		cooldown: 3500,
-		damage: 20,
-		health: 20,
-		attack: d, defend: d, neutral: d,
+	"-3": new PetalStat({ radius: 16, cooldown: 50, damage: 30, health: 30 }), // haha's dev petal
+	0: new PetalStat({ radius: 0, cooldown: 0, damage: 0, health: 0 }),
+	1: new PetalStat({ radius: 10, cooldown: 2500, damage: 10, health: 10 }), // basic
+	2: new PetalStat({ radius: 8, cooldown: 500, damage: 8, health: 5 }), // fast
+	3: new PetalStat({ radius: 12, cooldown: 5500, damage: 20, health: 20 }), // heavy 
+	4: new PetalStat({ radius: 6, cooldown: 6000, damage: 5, health: 5 }), // iris
+	7: new PetalStat({ radius: 11, cooldown: 3500, damage: 20, health: 20, // rose
 		post: (petal, centre, rad) => {
 			rad = Math.min(rad, C.normal);
     		petal.pubInfo.x = centre.x + Math.sin(petal.degree) * rad;
     		petal.pubInfo.y = centre.y + Math.cos(petal.degree) * rad;
-		},
-		equip: no
-
-    },
-
-    //faster
-    13: {
-		radius: 8,
-		cooldown: 500,
-		damage: 8,
-		health: 5,
-		attack: d, defend: d, neutral: d,
-		post: no,
-		equip: no
-	}
+		}}),
+	13: new PetalStat({ radius: 8, cooldown: 500, damage: 8, health: 5 }), // faster
 };
 
 module.exports = petalStats;
